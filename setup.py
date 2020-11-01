cbc5634c06c06411695daba702787bd483c31734
Mentha Yann Loris <ymentha@iccluster111.iccluster.epfl.ch>
2020-11-01 17:46:44 +0100

    Update common emojis

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='emojivec',
+    name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='A short description of the project.',
     author='Yann Mentha',
     license='MIT',
 )
