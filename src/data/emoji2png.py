34cbf7591d4b21508712f43abec754fd26064ef6
Yann Mentha <yann.mentha@gmail.com>
2020-11-26 09:38:24 +0100

    Added dictionary saving to the emoji2png framework

--- src/data/emoji2png.py
+++ src/data/emoji2png.py
@@ -4,7 +4,9 @@ Functions to convert emojis to png files
 
 from IPython.display import clear_output
 import pyscreenshot as ImageGrab
+from src.constants import PNG_PATH
 import emoji
+import pickle as pk
 import time
 from pathlib import Path
 
@@ -12,19 +14,21 @@ def print_and_capture(bbox,test=False,missing_emojis=None):
     """
     Allows to recreate the images from the emoji
     """
+    output_folder = PNG_PATH
     if missing_emojis is not None:
         emojis_dic = {key:val for key,val in emoji.EMOJI_UNICODE.items() if key.strip(":") in missing_emojis}
     else:
         emojis_dic = emoji.EMOJI_UNICODE
-    output_folder = Path("../data/processed/emojis_png/all/")
     if test:
-        for i,(label,em) in enumerate(emojis_dic.items()):
-            label = label.strip(":").replace(".","")
-            path = output_folder.joinpath(label).with_suffix(".png")
+        dic = {}
+        for i,(_,em) in enumerate(emojis_dic.items()):
+            path = output_folder.joinpath(str(i)).with_suffix(".png")
             print(em)
             time.sleep(0.15)
             im = ImageGrab.grab(bbox=bbox)
             im.save(path)
             clear_output()
+            dic[em] = i
+        pk.dump(dic,open(output_folder.joinpath("dic.pk"),"wb"))
     else:
         print("😃")
